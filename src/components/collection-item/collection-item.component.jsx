48f2108590aea9204c7f5a04474cd2d183d15cc7
Ramon742 <ramondavi-developer@outlook.com>
2020-05-09 11:06:23 -0300

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className='collection-item'>
-        <div className='image' 
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className='collection-footer'>
-            <span className='name'>{ name }</span>
-            <span className='price'>{ price }</span>
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
+    return (
+        <div className='collection-item'>
+            <div className='image' 
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className='collection-footer'>
+                <span className='name'>{ name }</span>
+                <span className='price'>{ price }</span>
+            </div>
+            <CustomButton onClick={() => addItem(item)} inverted> 
+                Add to Cart 
+            </CustomButton>
         </div>
-    </div>
-);
+    )
+};
 
-export default CollectionItem;
+//when we call this function, it'll receive the item as a property, passing to addItem action creator,
+//which return an object where the type is equal addItem and the payload is equal to item that passed in,
+//and then we'll dispatch this object into our store
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+//null is when we dont take any map states, props.
+export default connect(null, mapDispatchToProps)(CollectionItem);
